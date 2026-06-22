cd133d9b385b38480f85146ee0e422b5f993a1e0
ti2 <ti2@NB-TI2>
2026-06-22 14:20:20 -0300

    Interface atualizada

--- ValidadorDuplicidade/obj/Debug/net10.0-windows/ValidadorDuplicidade.AssemblyInfo.cs
+++ ValidadorDuplicidade/obj/Debug/net10.0-windows/ValidadorDuplicidade.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ValidadorDuplicidade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e21c372c084105b7cb995f50b9cdd97cfd85d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d240ea9c3499fc38d2f63fb97cca18ea571e6d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("ValidadorDuplicidade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ValidadorDuplicidade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
